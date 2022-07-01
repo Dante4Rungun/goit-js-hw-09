4160b459d27682c5dca277d52c02a50a775d5f14
Dante4Rungun <denischuprin9619@gmail.com>
2022-07-01 07:08:37 +0000

    Deploying to gh-pages from @ Dante4Rungun/goit-js-hw-09@acf7f8d8ca19897ab8d6401298851c83f2e522e8 🚀

--- 01-color-switcher.2555c8d4.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let o=null;t.addEventListener("click",(t=>{console.log("+"),o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(t=>{clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.2555c8d4.js.map
---
+++ 01-color-switcher.e0b6d0b9.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let o=null;e.addEventListener("click",(t=>{console.log("+"),o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0})),t.addEventListener("click",(t=>{clearInterval(o),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.e0b6d0b9.js.map
